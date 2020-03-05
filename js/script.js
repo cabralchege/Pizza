36affd9595c6b268437a2b55babbfc8489b87482
Cabral Chege <cabralowiro@gmail.com>
2020-03-05 10:27:24 +0300

    made changes to make the page work

--- js/script.js
+++ js/script.js
@@ -1,29 +1,15 @@
-// $(document).ready(function(){
-//     $("#flip").click(function(){
-//       $("#panel").slideDown("slow");
-//     });
-//   });
-  function getSize() {
-    var selectedValue = document.getElementById("size").value;
-    return parseInt(selectedValue);
-}
-function getCrust() {
-    var selectedCrust = document.getElementById("crusts").value;
-    return parseInt(selectedCrust);
-}
-function getToppings() {
-    var selectedToppings = document.getElementById("toppings").value;
-    return parseInt(selectedToppings);
-}
-function getQuantity() {
-    var selectedQuantity = document.getElementById("quantity").value;
-    return parseInt(selectedQuantity);
-}
 function getTotalAmount() {
-    var totalAmount = (getSize() + getCrust() + getToppings()) * getQuantity();
-    alert("You have Ordered" + getQuantity("")  +  " pizza."  +  ""  +  " The Total Amount is kshs "  +  (totalAmount)  +  ""  +  " Thank you for your order welcome again.");
-    prompt("Please enter your location")
-    alert("You delivery fee is 200/=")
-    alert("You order will be delivered shortly")
-    alert("Thank You for shoppping with us  .")
-}
+        var size = parseInt(document.getElementById("size").value)
+        var crust = parseInt(document.getElementById("crust").value)
+        var toppings = parseInt(document.getElementById("toppings").value)
+        var number = parseInt(document.getElementById("quantity").value)
+
+        var total =  number*(size + crust + toppings);
+        alert(" The Total Amount is kshs "  +  total  +  ""  +  " Thank you for your order welcome again.");
+        var loc=prompt("enter your location")
+            alert("You delivery fee is $200")
+            alert("You order will be delivered shortly to " + loc)
+            alert("Continue shoppping with us each and every time .")
+                 
+};      
+
