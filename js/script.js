059c49675f1b6bc8766b37881bff375079b3df8c
Cabral Chege <cabralowiro@gmail.com>
2020-03-05 10:43:04 +0300

    made the js calculate the total amount for getting the pizza

--- js/script.js
+++ js/script.js
@@ -5,11 +5,11 @@ function getTotalAmount() {
         var number = parseInt(document.getElementById("quantity").value)
 
         var total =  number*(size + crust + toppings);
-        alert(" The Total Amount is kshs "  +  total  +  ""  +  " Thank you for your order welcome again.");
-        var loc=prompt("enter your location")
-            alert("You delivery fee is $200")
+        alert(" The Total Amount is kshs "  +  total  +  ""  +  " Thank you for your order.");
+        var loc=prompt("Please enter your location")
+            alert("You delivery fee is 200kshs")
             alert("You order will be delivered shortly to " + loc)
-            alert("Continue shoppping with us each and every time .")
+            alert("Thank You for shoppping with us .")
                  
 };      
 
